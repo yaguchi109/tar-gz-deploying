8de4f723c0770794a39432442ed31e11cea378b7
yaguchi109 <yaguchi109@gmail.com>
2021-09-30 03:42:07 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-[ 0 -eq $# ] && echo "No arguments" exit 1
-wget --no-verbose --output-document=archive.tar.gz "$1" &&
-mkdir -p public &&
-tar -xzf archive.tar.gz -C public
+decompress () {
+  local TAR_FILE="archive.tar.gz"
+  local DEST_DIR="public"
+  wget --no-verbose --output-document="$TAR_FILE" "$1" &&
+  mkdir -p "$DEST_DIR" &&
+  tar -xzf "$TAR_FILE" -C "$DEST_DIR"
+  rm "$TAR_FILE"
+  rm -dr "$DEST_DIR"
+}
+
+FILE="$1"     && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE0" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE1" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE2" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE3" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE4" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE5" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE6" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE7" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE8" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
+FILE="$FILE9" && [ 0 -lt ${#FILE} ] && decompress "$FILE"
